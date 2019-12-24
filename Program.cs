58ac79bbfb43f261ed102b72717b603a46e4917e
dilaradeilim <dilaradst11@gmail.com>
2019-12-24 22:17:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazi.Kazan.Myo._3SubeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] sayilar = new int[3];
+            //sayilar[0] = 5;
+            //sayilar[1] = 3;
+            //sayilar[2] = 15;
+
+
+            //Console.WriteLine(sayilar[0]);
+            //Console.WriteLine(sayilar[1]);
+            //Console.WriteLine(sayilar[2]);
+
+            //Array.Sort(sayilar);
+
+            //for (int i = 0; i < 3; i++)
+            //{
+
+            //    Console.WriteLine(sayilar[i]);
+            //}
+            //kulanıcın girdiği isimleri alfebatik sırala
+
+            //Console.WriteLine("kaç adet isim gireceksiniz:");
+            //byte adet = byte.Parse(Console.ReadLine());
+            //string[] isim = new string[adet];
+
+            //for (int i = 0; i < adet; i++)
+            //{
+
+            //    Console.WriteLine("isim giriniz: ");
+            //    isim[i]=Console.ReadLine();
+
+            //}
+            //Array.Sort(isim);
+            //for (int i = 0; i < isim.Length; i++)
+            //{
+            //    Console.WriteLine(isim [i]);
+            //}
+
+            //Console.ReadKey();
+        }
+    }
+}
